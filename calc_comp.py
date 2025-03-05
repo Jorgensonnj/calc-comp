63bad9ea1d99ebe6769d187cc3aeddb1b152eb8b
Nicholas Jorgenson <nicholas.jorgenson@protonmail.com>
2025-03-05 16:10:15 -0500

    task: initial importing and reading of project set files, bug: reformatted set_4 to be consistent with other set files

--- calc_comp.py
+++ calc_comp.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python
 
+import os
+
+def import_and_process_files(directory: str):
+    # Loop through all set files in given directory
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+
+        # Open file for reading
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            print(f'{filename}:') # Print file's name
+            for index in range(len(lines)):
+                print(f'Line {index}: {lines[index].strip()}') # Print each individual line
+
+
 def main():
-    print("Tis just the beginning")
+
+    # Define where the sets of projects live relative
+    # to where this script gets executed
+    # example: /home/username/Projects/python/calc-comp/ as starting point
+    PROJECT_SETS_DIRECTORY = "sets"
+    PROJECT_SETS_PATH = os.path.join(os.path.dirname(__file__), PROJECT_SETS_DIRECTORY)
+
+    import_and_process_files(PROJECT_SETS_PATH)
+
+    #print("Tis just the beginning")
 
 if __name__ == "__main__":
     main()
