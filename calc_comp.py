ada5ce57750d903ba217d3ba0f96d6e803c085a6
Nicholas Jorgenson <nicholas.jorgenson@protonmail.com>
2025-03-06 09:38:24 -0500

    task: added calculate method to capture business logic, task: restructured output process_set_date method

--- calc_comp.py
+++ calc_comp.py
@@ -4,7 +4,7 @@ import os
 import re
 import heapq
 from datetime import datetime
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 
 def extract_project_data(line: str) -> Tuple[str, Dict[str, str]]:
     project_id = ""
@@ -17,28 +17,47 @@ def extract_project_data(line: str) -> Tuple[str, Dict[str, str]]:
     )
     if matches is not None:
         project_id = matches.group(1)
+        project_data["project_id"] = project_id
         project_data["project_cost"] = matches.group(2)
         project_data["project_start"] = matches.group(3)
         project_data["project_end"] = matches.group(4)
 
     return project_id, project_data
 
-def process_set_data(set_data: Dict[str, Dict[str, str]]):
+def process_set_data(set_data: Dict[str, Dict[str, str]]) -> List[Tuple[datetime, Dict[str, str]]]:
     dates = list()
     # Extract date date from dictionary and convert to datetime
     for project_data in set_data.values():
-        if (start_data := project_data.get("project_start")) and (end_data := project_data.get("project_end")):
-            dates.append(datetime.strptime(start_data, '%m/%d/%y')) # add start date to list
-            dates.append(datetime.strptime(end_data,'%m/%d/%y'))    # add end date to list
+        dates.append((datetime.strptime(project_data["project_start"], '%m/%d/%y'), project_data)) # add start date to list
+        dates.append((datetime.strptime(project_data["project_end"],'%m/%d/%y'), project_data))    # add end date to list
 
     # Where the magic happens
     heapq.heapify(dates)
 
-    while len(dates) > 0:
-        item = heapq.heappop(dates)
-        print(f"{item}")
+    return dates
 
+def calculate(date_heap: List[Tuple[datetime, Dict[str, str]]]):
 
+    # Hardcoded costs
+    city_cost = {"High": (85, 55), "Low": (75,45)}
+
+    total_compensation = 0
+
+    # Start pointer
+    start = heapq.heappop(date_heap)
+    start_date, start_data = start[0], start[1]
+
+    while len(date_heap) > 0:
+
+        end = heapq.heappop(date_heap)
+        end_date = end[0]
+
+        delta_days = (end_date - start_date).days + 1  # number of full days inbetween
+
+        start_cost = city_cost[start_data["project_cost"]]
+        total_compensation += ((delta_days - 2) * start_cost[0]) + (2 * start_cost[1])
+
+    return total_compensation
 
 def process_set(directory_path: str, file_name: str):
     file_path = os.path.join(directory_path, file_name)
@@ -47,7 +66,6 @@ def process_set(directory_path: str, file_name: str):
     # Open file and get data
     with open(file_path, 'r') as file:
         lines = file.readlines()
-        print(f"{file_name}:")
         for line in lines:
             project_id, project_data = extract_project_data(line.strip())
 
@@ -55,31 +73,31 @@ def process_set(directory_path: str, file_name: str):
             # This will prevent duplicate project information per set.
             set_data[project_id] = project_data
 
+    # After file is closed, process the set data
+    date_heap = process_set_data(set_data)
+    # Finally, calculate the compensation
+    total_compensation = calculate(date_heap)
 
-    # After file is closed,
-    process_set_data(set_data)
-    #print(f"{file_name}: {set_data}")
+    print(f"Set: {file_name} Total: {total_compensation}")
 
 def process_all(directory_path: str):
     # Loop through all set files in given directory
     for file_name in os.listdir(directory_path):
         process_set(directory_path, file_name)
 
-
 def main():
-
 
     DIRECTORY = "sets"
 
     # Define where the set files live relative to where this script gets executed
     DIRECTORY_PATH = os.path.join(os.path.dirname(__file__), DIRECTORY)
 
+    # Check if directory exists
     if not os.path.isdir(DIRECTORY_PATH):
         print(f"Directory does not exist.")
         exit()
 
     process_set(DIRECTORY_PATH, "set_1")
-    #process_all(DIRECTORY_PATH)
 
 
 if __name__ == "__main__":
