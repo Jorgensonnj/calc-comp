cadcd261db171098b78779fe302e551bde2e1749
Nicholas Jorgenson <nicholas.jorgenson@protonmail.com>
2025-03-06 06:59:11 -0500

    task: remove duplicate code

--- calc_comp.py
+++ calc_comp.py
@@ -28,29 +28,23 @@ def process_set_line(line: str) -> Dict[str, Dict[str, str]]:
 
     return set_data
 
-def process_set(directory: str, file_name: str):
-    # Open file for reading
+def process_set(directory_path: str, file_name: str):
+    file_path = os.path.join(directory_path, file_name)
 
-    file_path = os.path.join(os.path.dirname(__file__), directory, file_name)
-    print(file_path)
     set_data = dict()
+    # Open file and get data
     with open(file_path, 'r') as file:
         lines = file.readlines()
         for line in lines:
             set_data = process_set_line(line.strip())
 
-        print(f"{file_name}: {set_data}")
+    # After file is closed,
+    print(f"{file_name}: {set_data}")
 
-def process_all(directory: str):
-
-    # Define where the sets live relative
-    # to where this script gets executed
-    # example: /home/username/Projects/python/calc-comp/ as starting point
-    directory_path = os.path.join(os.path.dirname(__file__), directory)
-
+def process_all(directory_path: str):
     # Loop through all set files in given directory
     for file_name in os.listdir(directory_path):
-        process_set(directory, file_name)
+        process_set(directory_path, file_name)
 
 
 def main():
@@ -58,12 +52,15 @@ def main():
 
     DIRECTORY = "sets"
 
-    if not os.path.isdir(DIRECTORY):
+    # Define where the set files live relative to where this script gets executed
+    DIRECTORY_PATH = os.path.join(os.path.dirname(__file__), DIRECTORY)
+
+    if not os.path.isdir(DIRECTORY_PATH):
         print(f"Directory does not exist.")
         exit()
 
-    process_set(DIRECTORY, "set_1")
-    #process_all(DIRECTORY)
+    process_set(DIRECTORY_PATH, "set_1")
+    #process_all(DIRECTORY_PATH)
 
 
 if __name__ == "__main__":
