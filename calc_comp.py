1aa95e0224741d269e25bcf68e8ead91388c755d
Nicholas Jorgenson <nicholas.jorgenson@protonmail.com>
2025-03-06 23:36:49 -0500

    task: created the Project class, task: moved away from using heap logic and instead began merging projects based upon their start and end times in arrays

--- calc_comp.py
+++ calc_comp.py
@@ -6,96 +6,88 @@ import heapq
 from datetime import datetime
 from typing import Dict, Tuple, List
 
-def extract_project_data(line: str) -> Tuple[str, Dict[str, str]] | None:
+class Project:
+    def __init__(self, id: str, rate: str, start_date: datetime, end_date: datetime):
+        self.id = id
+        self.rate = rate
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def __repr__(self):
+        return f"[id: {self.id}, rate: {self.rate}, start: {self.start_date}, end: {self.end_date}]"
+
+    def __str__(self):
+        return f"[id: {self.id}, rate: {self.rate}, start: {self.start_date}, end: {self.end_date}]"
+
+
+def extract_project_data(line: str) -> Tuple[str, Project] | None:
     # Use regex to extract project information
     matches = re.search(
         r"Project (\d): (Low|High) Cost City Start Date: (\d?\d/\d?\d/\d\d) End Date: (\d?\d/\d?\d/\d\d)",
         line
     )
     if matches is not None:
-        project_data = dict()
+        project = Project(
+            matches.group(1),
+            matches.group(2),
+            datetime.strptime(matches.group(3), '%m/%d/%y'),
+            datetime.strptime(matches.group(4), '%m/%d/%y'),
+        )
 
-        project_id = matches.group(1)
-        project_data["project_id"] = project_id
-        project_data["project_cost"] = matches.group(2)
-        project_data["project_start"] = matches.group(3)
-        project_data["project_end"] = matches.group(4)
-
-        return project_id, project_data
+        return project.id, project
 
     return None
 
-def process_set_data(set_data: Dict[str, Dict[str, str]]) -> List[Tuple[datetime, str, Dict[str, str]]]:
-    dates = list()
-    # Extract date date from dictionary and convert to datetime
-    for project_data in set_data.values():
-        dates.append((
-            datetime.strptime(project_data["project_start"], '%m/%d/%y'),
-            project_data["project_id"],
-            project_data
-        )) # add start date to list
-        dates.append((
-            datetime.strptime(project_data["project_end"], '%m/%d/%y'),
-            project_data["project_id"],
-            project_data
-        )) # add start date to list
+def process_set_data(set_data: Dict[str, Project]) -> List[Project]:
+    projects = list()
 
-    # Where the magic happens
-    heapq.heapify(dates)
+    # Garrentee that the projects are in order of their start date
+    sorted_projects = [ item for item in sorted(set_data.values(), key=lambda project : project.start_date) ]
 
-    return dates
+    previous = sorted_projects[0]
+    for project in sorted_projects[1:]:
 
-def calculate(date_heap: List[Tuple[datetime, str, Dict[str, str]]]):
+        # merge projects into on larg
+        if project.start_date <= previous.end_date:
+            previous.end_date = max(previous.end_date, project.end_date)
+        else:
+            projects.append(previous)
+            previous = project
 
+    projects.append(previous)
+
+    return projects
+
+def calculate(projects: List[Project]):
     # Hardcoded costs
-    city_cost = {"High": (85, 55), "Low": (75,45)} # (full_day, travel_day)
+    #city_cost = {"High": (85, 55), "Low": (75,45)} # (full_day, travel_day)
 
     total_compensation = 0
-
-    left = heapq.heappop(date_heap) # first pointer
-    while len(date_heap) > 0:
-        right = heapq.heappop(date_heap) # last pointer
-
-        current_rate = city_cost[left[2]["project_cost"]]
-        full_rate   = current_rate[0]
-        travel_rate = current_rate[1]
-
-        while len(date_heap) > 0 and right[0] > date_heap[0][0]:
-
-            right_date = right[0]
-            next_date  = date_heap[0][0]
-
-            if len(date_heap) > 0 and right_date > next_date:
-                right = date_heap[0]
-
-        full_days = (right[0] - left[0]).days - 1
-
-        total_compensation += full_days * full_rate # full days
-        total_compensation += 2 * travel_rate # full days
 
     return total_compensation
 
 def process_set(directory_path: str, file_name: str):
     file_path = os.path.join(directory_path, file_name)
 
-    set_data = dict()
+    set_data: Dict[str, Project] = dict()
     # Open file and get data
     with open(file_path, 'r') as file:
         lines = file.readlines()
         for line in lines:
             result = extract_project_data(line.strip())
             if result:
-                project_id, project_data = result[0], result[1]
+                project_id, project = result[0], result[1]
                 # This will overwrite existing data at project_id location.
                 # This will prevent duplicate project information per set.
-                set_data[project_id] = project_data
+                set_data[project_id] = project
 
     # After file is closed, process the set data
-    date_heap = process_set_data(set_data)
-    # Finally, calculate the compensation
-    total_compensation = calculate(date_heap)
+    projects = process_set_data(set_data)
 
-    print(f"Set: {file_name} Total: {total_compensation}")
+    # Finally, calculate the compensation
+    #total_compensation = calculate(date_heap)
+
+    #print(f"Set: {file_name} Total: {total_compensation}")
 
 def process_all(directory_path: str):
     # Loop through all set files in given directory
@@ -114,8 +106,8 @@ def main():
         print(f"Directory does not exist.")
         exit()
 
-    #process_set(DIRECTORY_PATH, "set_2")
-    process_all(DIRECTORY_PATH)
+    process_set(DIRECTORY_PATH, "set_2")
+    #process_all(DIRECTORY_PATH)
 
 
 if __name__ == "__main__":
